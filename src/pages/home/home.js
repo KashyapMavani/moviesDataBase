94262fe81d1a8cb79690f77c226fda7c7ddb92c2
KashyapMavani <kmavani1807@gmail.com>
2023-04-13 21:37:34 +0530

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/components/card/card.css 	new file:   src/components/card/card.js 	new file:   src/components/header/Header.css 	new file:   src/components/header/Header.js 	new file:   src/components/movieList/movieList.css 	new file:   src/components/movieList/movieList.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/pages/home/home.css 	new file:   src/pages/home/home.js 	new file:   src/pages/movieDetail/movie.css 	new file:   src/pages/movieDetail/movie.js

---
+++ src/pages/home/home.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react"
+import "./home.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from "react-router-dom";
+import MovieList from "../../components/movieList/movieList";
+
+const Home = () => {
+
+    const [ popularMovies, setPopularMovies ] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/trending/all/week?api_key=ad35a5f915779125b014885cae98c1a9&language=en-US")
+        .then(res => res.json())
+        .then(data => setPopularMovies(data.results))
+    }, [])
+
+    return (
+        <>
+            <div className="poster">
+                <Carousel
+                    showThumbs={false}
+                    autoPlay={true}
+                    transitionTime={3}
+                    infiniteLoop={true}
+                    showStatus={false}
+                >
+                    {
+                        popularMovies.map(movie => (
+                            <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`} >
+                                <div className="posterImage">
+                                    <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
+                                </div>
+                                <div className="posterImage__overlay">
+                                    <div className="posterImage__title">{movie?.title || movie?.name || movie?.original_name}</div>
+                                    <div className="posterImage__runtime">
+                                        {movie ? movie.release_date : ""}
+                                        <span className="posterImage__rating">
+                                            {movie ? movie.vote_average :""}
+                                            <i className="fas fa-star" />{" "}
+                                        </span>
+                                    </div>
+                                    <div className="posterImage__description">{movie ? movie.overview : ""}</div>
+                                </div>
+                            </Link>
+                        ))
+                    }
+                </Carousel>
+                <MovieList />
+            </div>
+        </>
+    )
+}
+
+export default Home
+// https://api.themoviedb.org/3/movie/top_rated?api_key=ad35a5f915779125b014885cae98c1a9&language=en-US
+// movie ? movie.original_title: ""
+
+/*
+fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`, */
